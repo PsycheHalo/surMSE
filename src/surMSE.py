990d2894fa1e1f75bcfd1377ba07e84d8604e1b5
PsycheHalo <psyche@winsgod.com>
2026-04-26 16:12:11 +0800

    Update surMSE.py

--- src/surMSE.py
+++ src/surMSE.py
@@ -2,16 +2,19 @@ import torch
 import math
 
 
-def surMSE(input,target,dim=None,scale=False,eps=1e-8,meanOut=True,haveBatch=True):
+def surMSE(input,target,dim=None,scale=True,eps=1e-8,meanOut=True,haveBatch=True):
     if dim is None and haveBatch:
         dim=tuple(range(1,len(torch.broadcast_shapes(input.shape,target.shape))))
     
     inputNormSquare=torch.linalg.norm(input,ord=2,dim=dim,keepdim=False).square()
-    targetNormSquare=torch.linalg.norm(target,ord=2,dim=dim,keepdim=False).square()
+    targetNorm=torch.linalg.norm(target,ord=2,dim=dim,keepdim=False)
+    targetNormSquare=targetNorm.square()
     dot=(input*target).sum(dim=dim,keepdim=False)
     
     loss=(inputNormSquare*targetNormSquare-dot.square())+(targetNormSquare-dot).clamp(min=0)
     if scale:
+        loss=loss/(targetNorm+eps)
+    else:
         loss=loss/(targetNormSquare+eps)
     
     if meanOut:
