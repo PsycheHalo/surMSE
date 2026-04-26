63d8a8ae49e8a3d1e1468b4653e5484c9c39f895
PsycheHalo <psyche@winsgod.com>
2026-04-26 15:55:45 +0800

    Update surMSE.py

--- src/surMSE.py
+++ src/surMSE.py
@@ -2,9 +2,9 @@ import torch
 import math
 
 
-def orientLoss(input,target,dim=None,scale=False,eps=1e-8,meanOut=True,haveBatch=True):
+def surMSE(input,target,dim=None,scale=False,eps=1e-8,meanOut=True,haveBatch=True):
     if dim is None and haveBatch:
-        dim=tuple(range(1,len(torch.broadcast_shapes(input,target))))
+        dim=tuple(range(1,len(torch.broadcast_shapes(input.shape,target.shape))))
     
     inputNormSquare=torch.linalg.norm(input,ord=2,dim=dim,keepdim=False).square()
     targetNormSquare=torch.linalg.norm(target,ord=2,dim=dim,keepdim=False).square()
@@ -12,7 +12,7 @@ def orientLoss(input,target,dim=None,scale=False,eps=1e-8,meanOut=True,haveBatch
     
     loss=(inputNormSquare*targetNormSquare-dot.square())+(targetNormSquare-dot).clamp(min=0)
     if scale:
-      loss=loss/(targetNormSquare+eps)
+        loss=loss/(targetNormSquare+eps)
     
     if meanOut:
         return loss.mean()
